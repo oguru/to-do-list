86384f2f0289c8d466ca7b3b068f360ee17e3880
oguru <oguru@hotmail.co.uk>
2020-04-09 15:19:53 +0100

    Updates

---
+++ precache-manifest.910a083233651350910f71e11ded2742.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e708ed67ea109a5adc7366f810aabfd",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "820484a45c8479e6d7ed",
+    "url": "/to-do-list/static/css/main.0578b88d.chunk.css"
+  },
+  {
+    "revision": "673bc5e0de910d908080",
+    "url": "/to-do-list/static/js/2.2709c142.chunk.js"
+  },
+  {
+    "revision": "bad6e083d4d2a996fe60e513745a4089",
+    "url": "/to-do-list/static/js/2.2709c142.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "820484a45c8479e6d7ed",
+    "url": "/to-do-list/static/js/main.b6539414.chunk.js"
+  },
+  {
+    "revision": "ef1ab25b4db95c178911",
+    "url": "/to-do-list/static/js/runtime-main.7557c059.js"
+  },
+  {
+    "revision": "a6798ee2f578c10dc659a0dde7e7959c",
+    "url": "/to-do-list/static/media/main-postit.a6798ee2.svg"
+  },
+  {
+    "revision": "3a4b538f7f8e9afb899cb5d8edbe6c21",
+    "url": "/to-do-list/static/media/postit-blue.3a4b538f.svg"
+  },
+  {
+    "revision": "3a8010467ba14e95b420cc8738ad8c60",
+    "url": "/to-do-list/static/media/postit-green.3a801046.svg"
+  },
+  {
+    "revision": "8295d182cf6913ebc7907014f84b061b",
+    "url": "/to-do-list/static/media/postit-orange.8295d182.svg"
+  },
+  {
+    "revision": "c205b4c218de7388c8f2990c9340474b",
+    "url": "/to-do-list/static/media/postit-pink.c205b4c2.svg"
+  }
+]);
